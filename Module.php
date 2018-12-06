9f0660af14cd335d0975055e939e9310ea41ce5a
Mustafa Dinç <30298206+mustafadincmd@users.noreply.github.com>
2018-12-06 13:42:36 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -15,43 +15,7 @@ class Module extends \kouosl\base\Module
     public function init()
     {
         parent::init();
-
-        // custom initialization code goes here
     }
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        switch ($this->namespace)
-        {
-            case 'backend':{
-
-            };break;
-            case 'frontend':{
-
-            };break;
-            case 'api':{
-
-                $behaviors['authenticator'] = [
-                    'class' => CompositeAuth::className(),
-                    'authMethods' => [
-                        HttpBasicAuth::className(),
-                        HttpBearerAuth::className(),
-                        QueryParamAuth::className(),
-                    ],
-                ];
-            };break;
-            case 'console':{
-
-            };break;
-            default:{
-                throw new HttpException(500,'behaviors'.$this->namespace);
-            };break;
-        }
-
-        return $behaviors;
-
-    }
-
     public function registerTranslations()
     {
         Yii::$app->i18n->translations['site/*'] = [
@@ -63,12 +27,10 @@ class Module extends \kouosl\base\Module
             ],
         ];
     }
-
     public static function t($category, $message, $params = [], $language = null)
     {
         return Yii::t('sample/' . $category, $message, $params, $language);
     }
-
     public static function initRules(){
 
         return $rules = [
@@ -80,15 +42,7 @@ class Module extends \kouosl\base\Module
                 'tokens' => [
                     '{id}' => '<id:\\w+>'
                 ],
-                /*'patterns' => [
-                    'GET new-action' => 'new-action'
-                ]*/
             ],
-
         ] ;
-
     }
-
-
-
 }
